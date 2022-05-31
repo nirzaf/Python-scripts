8f41d87115fe178038de15a5ca5765417801e988
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 21:57:07 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 09:57:07 PM

--- .history/email_service_20220531215700.py
+++ .history/email_service_20220531215700.py
@@ -21,6 +21,7 @@ data = {
                 }
             ],
             
+
             "Subject": "My first Mailjet Email!",
             "TextPart": "Greetings from Mailjet!",
             "HTMLPart": "<h3>Dear Fazrin, welcome to <a href=\"quadrate.lk\">Mailjet</a>!</h3><br/><p style=\"color:red\"> May Allah's peace be with you!"
