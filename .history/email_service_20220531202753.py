d9e26de3331df18ece64509eadb171eb6a59a935
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 20:28:02 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 08:28:02 PM

--- .history/email_service_20220531202753.py
+++ .history/email_service_20220531202753.py
@@ -5,4 +5,4 @@ conn.ehlo()
 conn.starttls()
 conn.login('quadrate.lk@gmail.com', '4Chy6Q^)G.SUy49')
 conn.sendmail("quadrate.lk@gmail.com", "mfmfazrin1986@gmail.com", 'Subject: Test...... \n\n This is a sample email')
-conn.quit()
+con
