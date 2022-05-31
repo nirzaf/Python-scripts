88fdda4ecdff6b8b8f5d3130f9c1720c0fcf86b3
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 20:17:59 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 08:17:59 PM

--- .history/email_service_20220531201750.py
+++ .history/email_service_20220531201750.py
@@ -2,5 +2,3 @@ import smtplib
 
 conn = smtplib.SMTP('smtp.gmail.com', 587)
 conn.ehlo()
-conn.starttls()
-conn.login('username', 'password')
--- .history/email_service_20220531201755.py
+++ .history/email_service_20220531201755.py
@@ -3,4 +3,3 @@ import smtplib
 conn = smtplib.SMTP('smtp.gmail.com', 587)
 conn.ehlo()
 conn.starttls()
-conn.login('username', 'password')
