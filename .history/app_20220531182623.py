ed5aedeb8b09bd50f55d794d4c8b2e1e7c1aac30
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 18:26:39 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 06:26:39 PM

--- .history/app_20220531182623.py
+++ .history/app_20220531182623.py
@@ -4,7 +4,7 @@ import re, pyperclip
 
 #TODO: Create a regex for phone numbers
 phoneRegex = re.compile(r'''
-# 415-555-0000, 
+# 415-555-0000,
 ''', re.VERBOSE)
 
 #TODO: Create a regex for email addresses
