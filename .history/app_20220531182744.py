5a55ae2b282801f03826ca67602fb59618b0ab2c
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 18:27:59 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 06:27:59 PM

--- .history/app_20220531182744.py
+++ .history/app_20220531182744.py
@@ -7,7 +7,7 @@ phoneRegex = re.compile(r'''
 # 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345 
 
 #area code (optional)
-#first 
+
 ''', re.VERBOSE)
 
 #TODO: Create a regex for email addresses
