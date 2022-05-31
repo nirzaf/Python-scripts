7665fc72ee94de805213060f03fc38e2fa81a60e
nirzaf <mfmfazrin1986@gmail.com>
2022-05-31 20:26:42 +0530

    pushed by mfmfazrin1986@gmail.com at Tuesday, May 31, 2022, 08:26:42 PM

--- .history/email_service_20220531202632.py
+++ .history/email_service_20220531202632.py
@@ -3,4 +3,4 @@ import smtplib
 conn = smtplib.SMTP('smtp.gmail.com', 587)
 conn.ehlo()
 conn.starttls()
-conn.login('quadrate.lk@gmail.com', '4Chy6Q^)G.SUy49')
+conn.login('quadrate.lk@gmail.com', 'password')
